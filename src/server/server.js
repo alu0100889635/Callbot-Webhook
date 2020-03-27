9f120bfb21b1489dfe1adcc4d059a4e597a16f2a
Alba Cruz Torres <albacruztorres@gmail.com>
2020-03-27 11:46:25 +0000

    Se cambia la implementación de la función post

--- src/server/server.js
+++ src/server/server.js
@@ -10,7 +10,56 @@ app.use(cors()); */
 
 //app.use("/answers", answersRouter);
 
-app.post("/answers", function(req, res) {
+app.post("/questions", function(req, res) {
+	axios({
+		method: 'POST',
+		url: 'https://api.dialogflow.com/v1/query?v=callbot-271218',
+		headers: {
+			'Authorization': 'Bearer 1063cfb4d4cd4416b6f510cba3936f1e',
+			'Content-Type': 'application/json',
+		},
+		
+		data: {
+			"lang": "es",
+			"query": "hola",
+			"sessionId": "12345",
+		}
+	})  
+	.then((response) => {
+		console.log(response.data);
+	})
+	.catch((error) => {
+		console.log(error);                
+	})
+	/* const speech = req;
+	console.log(speech.body);
+  
+	const speechResponse = {
+		google: {
+		expectUserResponse: true,
+		richResponse: {
+			items: [
+			{
+				simpleResponse: {
+				textToSpeech: speech
+				}
+			}
+			]
+		}
+		}
+	};
+  
+	return res.json({
+		payload: speechResponse,
+		//data: speechResponse,
+		fulfillmentText: speech,
+		speech: speech,
+		displayText: speech,
+		source: "webhook-echo-sample"
+	}); */
+});
+
+/* app.get("/answers", function(req, res) {
     const speech = req.body.queryResult;
 	console.log(speech);
 
@@ -39,7 +88,7 @@ app.post("/answers", function(req, res) {
 		displayText: speech2,
 		source: "webhook-echo-sample"
 	});
-});
+}); */
 /* mongoose
 .connect("mongodb://localhost:27017/emergenciasdb", { useNewUrlParser: true, useUnifiedTopology: true })
 .then(console.log("DB connection was succesful"))
