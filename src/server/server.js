4bdbe5d0372c5f6dd3135f2af21298ae3507bfc4
Alba Cruz Torres <albacruztorres@gmail.com>
2020-03-30 10:21:23 +0100

    Se realiza una sentencia if para diferenciar entre la bienvenida y las demás preguntas

--- src/server/server.js
+++ src/server/server.js
@@ -12,9 +12,25 @@ app.use(cors()); */
 
 //app.use("/answers", answersRouter);
 
+//             1ªpreg,2ªpreg,3ªpreg,4ªpreg,5ªpreg  Array de preguntas
+let contestadas = [false, false, false, false, false];
+const bienvenida = "Hola, voy a guiarle a través de este test sobre el coronavirus (covid19).\n\nSi usted presenta una emergencia contacte con el 112.\n\nEste test no es un sustituto de un consejo, diagnóstico o tratamiento médico profesional. Consulte siempre a un profesional sobre síntomas serios u otro tipo de emergencias. ¿Desea comenzar?"
+const pregunta1 = "¿Ha viajado recientemente a alguno de los siguientes países?.España: las Comunidades Autónomas de Madrid y de La Rioja o los municipios de La Bastida y Vitoria (País Vasco). China: todas las provincias, incluyendo Hong Kong y Macao. Italia: todo el país. Francia: departamentos de Haut-Rhin (Grabd Est) y l\'Oise (Hauts-de-France). Alemania: departamento de Heinsberg (Renania del Norte-Westfalia)  Japón: isla de Hokkaidō. Corea del Sur. Singapur o Irán.";
+const pregunta2 = "¿Ha estado en contacto con alguien que haya viajado a estos países y que ahora esté enfermo?";
+const pregunta3 = "¿Ha estado en contacto con alguien que conozca que esté contagiado de coronavirus (covid19)?";
+const pregunta4 = "¿Algún profesional sanitario le ha manifestado que podría haber estado expuesto al coronavirus (covid19)?";
+const pregunta5 = "¿Presenta alguno de los siguientes síntomas? ¿Fiebre, Tos, Moqueo nasal o Dolor de garganta?";
+let speech = "";
+
 app.post("/questions", function(req, res) {
-	const speech = req.body.queryResult.fulfillmentText;//.parameters.RecentlyTraveled;
+	const queryText = req.body.queryResult.queryText;//.parameters.RecentlyTraveled;
 	console.log(req.body);
+	if(queryText == "hola"){
+		speech = bienvenida;
+	}
+	else {
+		speech = "No te entiendo";
+	}
 	const speechResponse = {
 		google: {
 			expectUserResponse: true,
