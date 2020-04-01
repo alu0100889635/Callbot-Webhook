a7f6ebc1963b08822e2f1aab64847255151bd17e
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-01 11:32:20 +0100

    Se añaden middlewares y mongoose

--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,15 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const testRouter = require("../routers/testRouter");
+const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Expose-Headers', 'x-total-count');
@@ -14,10 +19,13 @@ app.use((req, res, next) => {
   
     next();
 });
-/* app.use(morgan("tiny"));
-app.use(cors()); */
 
 app.use("/test", testRouter);
+
+mongoose
+.connect("mongodb://localhost:27017/emergenciasdb", { useNewUrlParser: true, useUnifiedTopology: true })
+.then(console.log("DB connection was succesful"))
+.catch(err => console.log(err));
 
 app.set("port", process.env.PORT || 8000);
 const port = app.get("port");
