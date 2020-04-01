0a90459aa13c1031e0d262f0dd11dfb70d831da6
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-01 11:54:47 +0100

    Se cambia la manera de llamar a la bd

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -2,6 +2,7 @@ const questions = require("./questions.json");
 const axios = require("axios");
 let answers = require("./answers.json");
 let subject = require("./subject.json");
+const URL = "http://localhost:3000";
 
 const cases = (intent, parameters) => {
 	switch(intent){
@@ -112,4 +113,53 @@ module.exports.postTest = function (req, res) {
 		displayText: speech,
 		source: "webhook-echo-sample"
 	});
+}
+
+const sendPhonecallToDB =  async () => {
+
+	axios.post(URL + "/phonecalls/addPhonecall", answers)
+	.then(response => console.log(response))
+	.catch(e => console.log(e));
+}
+//comentario
+/* async function sendSubjectToDB(){
+	console.log("estamos dentro de sendSUbjectToDB");
+	axios.post("http://[::1]:3000/subjects/addSubject", subject)
+	.then(response => {
+		console.log(response);
+		answers.subject_id = response;
+	})
+	.catch(e => console.log(e));
+
+} */
+
+const sendSubjectToDB = async () => {
+
+	axios.post(URL + "/subjects/addSubject", subject)
+	.then(response => {
+		console.log(response);
+		answers.subject_id = response;
+	})
+	.catch(e => console.log(e));
+}
+
+const sendDataToDB = async () => {
+
+	console.log("entra en sendDataDB");
+	
+	let respuesta1 = await sendSubjectToDB();
+	console.log("respuesta sujeto = ", respuesta1);
+	let respuesta2 = await sendPhonecallToDB();
+	console.log("respuesta llamada = ", respuesta2);
+}
+const sendToDB = async () => {
+
+		let resultado = await sendDataToDB();
+		console.log("Resultado de sendtodatabase", resultado);
+
+}
+
+
+module.exports.postData = async function (req, res) {
+	await sendToDB();
 }
--- src/routers/testRouter.js
+++ src/routers/testRouter.js
@@ -5,6 +5,7 @@ const testController = require("../controllers/testController");
 /* router.get("/", phonecallController.showAll);
 router.get("/:id", phonecallController.showPhonecall); */
 router.post("/", testController.postTest);
+router.post("/addData", testController.postData);
 /* router.delete("/:id", phonecallController.deletePhonecall);
 router.put("/:id", phonecallController.updatePhonecall);
  */
