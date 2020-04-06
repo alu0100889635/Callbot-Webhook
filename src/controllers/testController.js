a91fee64edf99f1c568b69a758e5cbb6c5519bd5
Alba Cruz Torres <albacruztorres@gmail.com>
2020-04-06 13:09:58 +0100

    Se intenta manejar el Default Fallback intent

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -119,7 +119,7 @@ const cases = async (intent, parameters) => {
 			await sendSubjectToDB();
 			return questions.pregunta10;
 		case "1pregunta - fallback":
-			return questions.pregunta1;
+			return questions.fallback + questions.pregunta1;
 			
 		default:
 			console.log(intent);
